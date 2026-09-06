5d60fa4253a84c8c89c11ba0fa91cfb593493f21
LOHAN YURI <166454706+LohanYuri@users.noreply.github.com>
2026-09-06 08:12:49 -0400

    Conectar portal ao Supabase

---
+++ assets/js/supabase-config.js
@@ -0,0 +1,3 @@
+window.ALDEIA_SUPABASE_URL="https://omlcissqryhckiwmtudi.supabase.co";
+window.ALDEIA_SUPABASE_KEY="sb_publishable_YWICYWihdeKwRsdCH8RCZQ_lHyRsZky";
+window.ALDEIA_SUPABASE=window.supabase.createClient(window.ALDEIA_SUPABASE_URL,window.ALDEIA_SUPABASE_KEY);
